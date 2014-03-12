a349e210ca46206183fcf42edf4b25bc3c48f42c
Charles Northup <cjnorth40@gmail.com>
2014-03-12 12:15:27 -0500

    Initial Commit

---
+++ MySafari/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySafari
+//
+//  Created by Charles Northup on 3/12/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
